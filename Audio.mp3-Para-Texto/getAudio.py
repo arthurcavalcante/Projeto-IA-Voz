2ae4cd49acee6bcc42f01a790d2ac3fbf339ab75
arthurguaritabrasil <arthurguaritabrasil07@gmail.com>
2024-10-01 13:20:51 -0300

    getAudio and play it to test

---
+++ Audio.mp3-Para-Texto/getAudio.py
@@ -0,0 +1,49 @@
+#
+#
+# <  pip install sounddevice numpy soundfile  >
+#
+#
+
+import sounddevice as sd
+import numpy as np
+import soundfile as sf
+import tempfile
+import os
+
+def gravar_audio(duracao=10, taxa_amostragem=44100, canais=1):
+    print("Gravando...")
+    audio_data = []
+    
+    def callback(indata, frames, time, status):
+        if status:
+            print(status)
+        audio_data.append(indata.copy())
+    
+    with sd.InputStream(samplerate=taxa_amostragem, channels=canais, callback=callback):
+        input("Pressione ENTER para parar a gravação.")
+    
+    audio_array = np.concatenate(audio_data, axis=0)
+    return audio_array, taxa_amostragem
+
+def salvar_audio_em_arquivo(audio_array, taxa_amostragem):
+
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio_file:
+        sf.write(temp_audio_file.name, audio_array, taxa_amostragem)
+        temp_audio_file_path = temp_audio_file.name
+    
+    print(f"Áudio salvo em: {temp_audio_file_path}")
+    return temp_audio_file_path
+
+def reproduzir_audio(file_path):
+    data, fs = sf.read(file_path)
+    print("Reproduzindo áudio...")
+    sd.play(data, fs)
+    sd.wait()
+
+def main():
+    audio_array, taxa_amostragem = gravar_audio()
+    file_path = salvar_audio_em_arquivo(audio_array, taxa_amostragem)
+    reproduzir_audio(file_path)     # testar o audio
+    os.remove(file_path)        # salva o arquivo e dps remove
+
+main()
